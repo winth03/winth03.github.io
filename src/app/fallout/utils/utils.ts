e256edce56ddd7ab9884f0560b9f48a898e29709
Nutpapop Yasawut <66255462+winth03@users.noreply.github.com>
2024-06-27 21:49:52 +0700

    refactor: Update junk.csv and utils.ts

--- src/app/fallout/utils/utils.ts
+++ src/app/fallout/utils/utils.ts
@@ -1,5 +1,5 @@
 export function toTitleCase(str: string) {
-    str = str?.split("\\").pop()?.split("/").pop()?.replace(/_/gi, " ").split(".")[0] ?? "";
+    str = str?.split("\\").pop()?.split("/").pop()?.replace(/_/gi, " ") ?? "";
     return str.replace(
         /\w\S*/g,
         function (txt) {
